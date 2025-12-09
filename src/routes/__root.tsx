5aca67880cf26ec12ea4bab91a0d1505a8b267a8
MAC <ahmed.hassaine432@gmail.com>
2025-12-09 18:25:18 +0100

    added the thing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convexUrl = import.meta.env.VITE_CONVEX_URL as string
+const convexUrl = "https://modest-hawk-185.convex.cloud"
 const convex = new ConvexReactClient(convexUrl);
 
 export const Route = createRootRoute({
